04ea1960cf257e23e26ffcb6e393d6d525cb1397
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-08-09 21:41:41 -0300

    chore: update version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # lib-release
 
+## 0.0.2
+
+### Patch Changes
+
+- add c and d
+
 ## 0.0.1
 
 ### Patch Changes
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const sum = (a: number, b: number, c: number) => {
-  return a + b + c
+const sum = (a: number, b: number, c: number, d: number) => {
+  return a + b + c + d
 }
 
 export {
