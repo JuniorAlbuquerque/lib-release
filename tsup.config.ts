f649faaa62347927e1a1e583a57235b1372c928d
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-08-09 23:40:45 -0300

    fix: lib

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  name: 'lib-release',
+  name: 'lib-release-new',
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   outDir: 'dist',
