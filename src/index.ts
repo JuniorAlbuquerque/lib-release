cb1b3c690743e468c533f54f0d11447d92a8473d
Junior Albuquerque <junioralbuquerque81@gmail.com>
2023-08-09 22:06:49 -0300

    chore: add multiply

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ const sum = (a: number, b: number, c: number) => {
   return a + b + c
 }
 
+const multiply = (a: number, b: number) => {
+  return a * b
+}
+
 export {
-  sum
+  sum,
+  multiply
 }
